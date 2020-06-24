c0e2cbef305d487df83805bae0aa2697818bfab8
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-06-24 22:18:17 +0900

    added tests

--- test_orbit_calculation.py
+++ test_orbit_calculation.py
@@ -6,41 +6,39 @@ script for unit testing orbit_calculation.py
 import unittest 
 from orbit_calculation import *
 
-class TestSQLResponse(unittest.TestCase):
-
-    def setUp(self):
-        self.x = fetch_sql(SAT_NUM)
-
-    def tearDown(self):
-        self.x = None 
-
-    def checkLength(self): # check that 5 values are returned from SQL database
-        self.assertEquals(len(self.x) == 5)
-
-    def checkName(self): # check if name is a string
-        self.assertTrue(isinstance(self.x[0], basestring))
-
+EARTH_R = 6.371*10**6
 
 
 class TestOrbitValues(unittest.TestCase):
 
     def setUp(self):
-        pass 
+        name, inclination, e, perigee, n = fetch_sql(SAT_NUM)
+        self.sat = Satellite(name, inclination, e, perigee, n)
 
     def tearDown(self):
-        pass 
+        self.sat = None
 
-    def testEccentricity(self):
-        pass 
+    def testCheckName(self):
+        # check if name is a string
+        self.assertIsInstance(self.sat.name, str)
 
-    def testPerigeeApogee(self):
-        pass 
+    def testEccentricity(self): 
+        #test eccentricity is not negative
+        self.assertGreaterEqual(self.sat.e, 0)
 
-    def testPeriod(self):
-        pass
+    def testPerigeeApogee(self): 
+        #test apogee and perigee larger than earth radius 
+        #test apogee equal to or greater than perigee
+        self.assertGreater(self.sat.r_a, EARTH_R)
+        self.assertGreater(self.sat.r_p, EARTH_R)
+        self.assertGreaterEqual(self.sat.r_a, self.sat.r_p) 
 
-    def 
+    def testInclination(self):
+        # test inclination between 0 and 180 degrees
+        self.assertTrue(self.sat.inclination>= 0 and self.sat.inclination <= 180)
+
+    
 
 
-if __name__=="__main__":
+if __name__== "__main__":
     unittest.main()
