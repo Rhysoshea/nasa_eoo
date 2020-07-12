871aeca4a986dd7ea22ff47d7bb49e747ea261c8
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-07-12 21:55:55 +0900

    added OrbitDisplay function to draw circles

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -159,7 +159,26 @@ class ItemDisplay extends React.Component {
 
 }
 
-// function OrbitDisplay 
+function OrbitDisplay (){
+
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 70;
+
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 500;
+    context.strokeStyle = '#003300';
+    context.stroke();
+
+    return (
+        <div></div>
+    )
+}
 
 
 // function ItemDisplay({ items }) {
