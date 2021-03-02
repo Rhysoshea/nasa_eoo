b742a3cdf6bf811662f972bcc8f65ff85e0ed701
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2021-03-02 23:08:52 +0900

    update

--- python_scripts/sql_info.py
+++
@@ -1,5 +0,0 @@
-host= "mysql"
-user= "root"
-password = "secret"
-db =  "eoo"
-sql_credentials = [host, user, password, db]
--- src/static/js/webglearth.maptiler.js
+++ src/static/js/webglearth.maptiler.js
@@ -1,223 +1,227 @@
-var gl;
-var pwgl = {
-};
+const gl = document.getElementById("myCanvas").getContext("webgl");
 
-pwgl.ongoingImageLoads = [];
-var canvas;
-// Variables for interactive control
-var transY = 0, transZ = 0;
-var xRot = yRot = zRot = xOffs = yOffs = drag = 0;
-pwgl.listOfPressedKeys = []; // Keep track of pressed down keys in a list
-var lastTime = 0;
+let aspectRatio;
+let currentRotation = [0, 1];
+let currentScale = [1.0, 1.0];
+
+// Vertex information
+
+let vertexArray;
+let vertexBuffer;
+let vertexNumComponents;
+let vertexCount;
+
+// Rendering data shared with the
+// scalers.
+
+let uScalingFactor;
+let uGlobalColor;
+let uRotationVector;
+let aVertexPosition;
+
+// Animation timing
+
+let previousTime = 0.0;
+let degreesPerSecond = 10.0;
 
 
-function createGLContext(canvas) {
-    var names = ["webgl", "experimental-webgl"];
-    var context = null;
-    for (var i = 0; i < names.length; i++) {
-        try {
-            context = canvas.getContext(names[i]);
-        }
-        catch (e) {
-        }
-        if (context) {
-            break;
-        }
+// const vertexShader = `
+//     attribute vec2 aVertexPosition;
+
+//     uniform vec2 uScalingFactor;
+//     uniform vec2 uRotationVector;
+
+//     void main() {
+//         vec2 rotatedPosition = vec2(
+//             aVertexPosition.x * uRotationVector.y +
+//                 aVertexPosition.y * uRotationVector.x,
+//             aVertexPosition.y * uRotationVector.y - 
+//                 aVertexPosition.x * uRotationVector.x
+//         );
+//         gl_Position = vec4(rotatedPosition * uScalingFactor, 0.0, 1.0);
+//     }
+// `
+
+const vertexShader = `
+    attribute vec2 aVertexPosition;
+
+    uniform vec2 uScalingFactor;
+    uniform vec2 uRotationVector;
+
+    void main() {
+        
+        gl_Position = vec4(aVertexPosition * uRotationVector * uScalingFactor, 0.0, 1.0);
+
     }
-    if (context) {
-        context.viewportWidth = canvas.width;
-        context.viewportHeight = canvas.height;
-    }
-    else {
-        alert("Failed to create WebGL context!");
-    }
-    return context;
-}
+`
 
+const fragmentShader = `
+    precision highp float;
 
-var vertexShaderSrc = 'attribute vec3 coordinates;' + 
-                      'void main(void) {' +
-                      ' gl_Position = vec4(coordinates, 1.0);' + 
-                      '}';
+  uniform vec4 uGlobalColor;
 
-var fragmentShaderSrc = 'void main(void) {' + 
-                        ' gl_FragColor = vec4(0, 0, 0, 1);' + 
-                        '}';
+  void main() {
+    gl_FragColor = uGlobalColor;
+  }
+    `;
 
 
 
 
-function degToRad(degrees) {
-    return degrees * Math.PI / 180;
-}
 
-function setupShaders() {
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderSrc);
-    gl.compileShader(vertexShader);
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderSrc);
-    gl.compileShader(fragmentShader);
-    // check shaders compiled
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
-        alert("Could not compile vertex shader: " + gl.getShaderInfoLog(vertexShader));
-    }
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        alert("Could not compile vertex shader: " + gl.getShaderInfoLog(fragmentShader));
-    }
+function animateScene() {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0.8, 0.9, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Could not initialise shaders " + gl.getProgramInfoLog(shaderProgram));
-    }
-
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    let radians = currentAngle * Math.PI / 180.0;
+    currentRotation[0] = Math.sin(radians);
+    currentRotation[1] = Math.cos(radians);
 
     gl.useProgram(shaderProgram);
-    gl.enableVertexAttribArray(positionAttributeLocation);
 
-    // pwgl.vertexPositionAttributeLoc = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    // gl.enableVertexAttribArray(pwgl.vertexPositionAttributeLoc);
-    // pwgl.vertexTextureAttributeLoc = gl.getAttribLocation(shaderProgram, "aTextureCoordinates");
-    // gl.enableVertexAttribArray(pwgl.vertexTextureAttributeLoc);
-    // pwgl.vertexNormalAttributeLoc = gl.getAttribLocation(shaderProgram, "aVertexNormal");
-    // gl.enableVertexAttribArray(pwgl.vertexNormalAttributeLoc);
-    // pwgl.uniformProjMatrixLoc = gl.getUniformLocation(shaderProgram, "uPMatrix");
-    // pwgl.uniformMVMatrixLoc = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-    // pwgl.uniformNormalMatrixLoc = gl.getUniformLocation(shaderProgram, "uNMatrix");
-    // pwgl.uniformSamplerLoc = gl.getUniformLocation(shaderProgram, "uSampler");
-    // pwgl.uniformLightPositionLoc = gl.getUniformLocation(shaderProgram,
-    //     "uLightPosition");
-    // pwgl.uniformAmbientLightColorLoc = gl.getUniformLocation(shaderProgram,
-    //     "uAmbientLightColor");
-    // pwgl.uniformDiffuseLightColorLoc = gl.getUniformLocation(shaderProgram,
-    //     "uDiffuseLightColor");
-    // pwgl.uniformSpecularLightColorLoc = gl.getUniformLocation(shaderProgram,
-    //     "uSpecularLightColor");
-    // pwgl.uniformSpotDirectionLoc = gl.getUniformLocation(shaderProgram, "uSpotDirection");
-    // pwgl.modelViewMatrix = mat4.create();
-    // pwgl.modelViewMatrixStack = [];
-    // pwgl.projectionMatrix = mat4.create();
+    uScalingFactor =
+        gl.getUniformLocation(shaderProgram, "uScalingFactor");
+    uGlobalColor =
+        gl.getUniformLocation(shaderProgram, "uGlobalColor");
+    uRotationVector =
+        gl.getUniformLocation(shaderProgram, "uRotationVector");
+
+    gl.uniform2fv(uScalingFactor, currentScale);
+    gl.uniform2fv(uRotationVector, currentRotation);
+    gl.uniform4fv(uGlobalColor, [0.1, 0.7, 0.2, 0.5]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    aVertexPosition =
+        gl.getAttribLocation(shaderProgram, "aVertexPosition");
+
+    gl.enableVertexAttribArray(aVertexPosition);
+    gl.vertexAttribPointer(aVertexPosition, vertexNumComponents,
+        gl.FLOAT, false, 0, 0);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertexCount);
+
+    window.requestAnimationFrame(function (currentTime) {
+        let deltaAngle = ((currentTime - previousTime) / 1000.0)
+            * degreesPerSecond;
+
+        currentAngle = (currentAngle + deltaAngle) % 360;
+
+        previousTime = currentTime;
+        animateScene();
+    });
 }
 
+function buildShaderProgram(shaderInfo) {
+    let program = gl.createProgram();
 
-function setupOrbitBuffers() {
+    shaderInfo.forEach(function (desc) {
+        let shader = compileShader(desc.id, desc.type);
 
-    orbitBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, orbitBuffer);
+        if (shader) {
+            gl.attachShader(program, shader);
+        }
+    });
 
-    const positions = [
-        0, 0,
-         0, 0.5,
-        0.7, 0
-    ];
+    gl.linkProgram(program)
 
-    gl.bufferData(gl.ARRAY_BUFFER, 
-                  new Float32Array(positions),
-                  gl.STATIC_DRAW);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.log("Error linking shader program:");
+        console.log(gl.getProgramInfoLog(program));
+    }
 
+    return program;
 }
 
+function compileShader(id, type) {
+    let code = id;
+    let shader = gl.createShader(type);
 
+    gl.shaderSource(shader, code);
+    gl.compileShader(shader);
 
-
-function setupBuffers() {
-    setupOrbitBuffers();
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.log(`Error compiling ${type === gl.VERTEX_SHADER ? "vertex" : "fragment"} shader:`);
+        console.log(gl.getShaderInfoLog(shader));
+    }
+    return shader;
 }
 
+function createCircle(radius, size) {
+    const numSlices = 360.0;
+    const vertexPerTriangle = 3.0;
+    const numVerts = numSlices * vertexPerTriangle;
+    // let radius = 0.5;
 
+    const arr = new Float32Array(numVerts+1);
+    for (i=0; i<numVerts+1; i=i+2) {
+        let sliceId = Math.floor(i/vertexPerTriangle);
+        let triVertexId = i % vertexPerTriangle;
+        let edge = triVertexId + sliceId;
+        let u = edge / numSlices;
+        let angle = u * Math.PI *2.0;
+        arr[i ] = ((Math.cos(angle)*size) + radius);
+        arr[i + 1] = ((Math.sin(angle)*size) + radius);
+    };
 
-function drawOrbit() {
-
-    const positions = [];
-
-    var r = 10;
-
-    // var center = (0, 0);
-    // positions.push(center);
-    // const colors = [
-    //     0.0, 0.0, 0.0, 1.0,
-    // ]
-
-    // const stops = 100;
-
-    // for (i=0; i<stops; i++){
-    //     positions.push( center + (
-    //         (r*Math.cos(i * 2 * Math.PI / stops)),
-    //         (r*Math.sin(i * 2 * Math.PI / stops))
-    //     ));
-
-    // }
-
-    var offset = 0;
-    var count =3;
-
-    console.log(positions);
-    // console.log(colors);
-    gl.drawArrays(gl.TRIANGLES, offset, count);
+    return arr;
 }
 
+function createSatellites() {
+    // let circleId =  Math.floor(vertexId / numVertsPerCircle);
+    let numCircles = 1;
 
-var newAngle = 0;
-function draw() {
-    // var currentTime = Date.now();
+    // let u = circleId / numCircles; // goes from 0 to 1
+    // float angle = u * PI * 2.0; // goes from 0 to 2PI
+    // float angle = 0.0;
+    let orbitRadius = 0.5;
+    let satRadius = 0.05;
 
-    // yRot = xRot = zRot = transY = transZ = 0;
+    // vec2 pos = vec2(cos(angle), sin(angle)) * orbitRadius;
+    // vec2 triPos = computeCircleTriangleVertex(vertexId) * satRadius;
 
-    // gl.uniform1i(pwgl.uniformSamplerLoc, 0);
-    // var timeNow = new Date().getTime();
-    // if (lastTime != 0) {
-    //     var elapsed = timeNow - lastTime;
-    //     newAngle += (90 * elapsed) / 1000;
-    // }
-    // lastTime = timeNow;
-    // var rotationSpeed = -degToRad(newAngle) / 50; // Sets earth rotation speed 
-    // if (currentTime === undefined) {
-    //     currentTime = Date.now();
-    // }
+    // gl_Position = vec4((pos + triPos)*scale, 0, 1);
 
-    // if (pwgl.animationStartTime === undefined) {
-    //     pwgl.animationStartTime = currentTime;
-    // }
-
-    // pwgl.angle = (currentTime - pwgl.animationStartTime) / pwgl.orbitalMultipler * 2 * Math.PI % (2 * Math.PI); // determines where in the orbit the satellite should be at a given time
-    // pwgl.x = Math.cos(pwgl.angle) * pwgl.circleRadius;
-    // pwgl.z = Math.sin(pwgl.angle) * pwgl.circleRadius;
-
-
-    // const offset = 0;
-    // const vertexCount = 4;
-    drawOrbit();
+    return createCircle(orbitRadius, satRadius);
 }
+
 
 function startup() {
-    var canvas = document.getElementById("myCanvas");
-    canvas = WebGLDebugUtils.makeLostContextSimulatingCanvas(canvas);
-    gl = createGLContext(canvas);
-    init();
-    draw();
-}
 
-var relation = 1.6 / 2500;
-function init() {
-    // Initialization that is performed during first startup and when the
-    // event webglcontextrestored is received is included in this function.
-    setupShaders();
-    setupBuffers();
 
-    // Transparent canvas for space background image
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
-    gl.enable(gl.DEPTH_TEST);
-    pwgl.x = 0.0;
-    pwgl.y = 0.0;
-    pwgl.z = 0.0;
-    pwgl.circleRadius = 10.0; // radius of satellite around Earth
-    pwgl.orbitalMultipler = 2000;
-    pwgl.satMultiplier = 1; // spin of satellite
+    const shaderSet = [
+        {
+            type: gl.VERTEX_SHADER,
+            id: vertexShader
+        },
+        {
+            type: gl.FRAGMENT_SHADER,
+            id: fragmentShader
+        }
 
+    ];
+
+    shaderProgram = buildShaderProgram(shaderSet);
+
+    aspectRatio = gl.canvas.width / gl.canvas.height;
+    currentRotation = [0, 1];
+    currentScale = [1.0, aspectRatio];
+
+    // vertexArray = createCircle(0);
+    vertexArray = createSatellites();
+    console.log(vertexArray);
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
+
+    vertexNumComponents = 2;
+    vertexCount = vertexArray.length / vertexNumComponents;
+    console.log(vertexCount);
+    currentAngle = 0.0;
+    rotationRate = 6;
+
+    animateScene();
 
 }
 
