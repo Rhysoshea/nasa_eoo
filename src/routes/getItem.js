50cdd7bf9109d41ec140718825ad3a529c693601
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2021-04-12 22:10:03 +0900

    changed nature of SQL fetch command

--- src/routes/getItem.js
+++ src/routes/getItem.js
@@ -1,6 +1,7 @@
 const db = require('../persistence');
 
 module.exports = async (req, res) => {
-    const name = req.body.name;
-    res.send(name);
+    // const name = req.body.name;
+    const items = await db.getItem(req.body.name);
+    res.send(items);
 };
